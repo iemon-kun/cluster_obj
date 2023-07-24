d2fb58fdabf3fb7d91b9bd1cdacb4ff712e3d7af
iemon_kun <74810160+iemon-kun@users.noreply.github.com>
2023-07-25 02:03:37 +0900

    Update StageFrontLight.js

--- craft_item/StageFrontLight.js
+++ craft_item/StageFrontLight.js
@@ -1,71 +1,47 @@
 // 子オブジェクトを取得
 const rootObj = $.subNode("Lightroot");
 const blueObj = $.subNode("Blue");
-const redObj= $.subNode("Red");
+const redObj = $.subNode("Red");
 const greenObj = $.subNode("Green");
 
 // 定数などの設定
-let count = $.state.count;
-if(count == null) count = 0;
+let count = $.state.count || 0;
 
-//インタラクト時の処理
+// オブジェクトと関数の設定
+const objStateMap = [
+  { obj: rootObj, stateKey: "rootOnOff" },
+  { obj: blueObj, stateKey: "blueOnOff" },
+  { obj: redObj, stateKey: "redOnOff" },
+  { obj: greenObj, stateKey: "greenOnOff" },
+];
+
+function settingEnabled(obj, stateKey, enabled) {
+  obj.setEnabled(enabled);
+  $.state[stateKey] = enabled;
+}
+
+// インタラクト時の処理
 $.onInteract(() => {
-  count += 1;
+  count = (count + 1) % 5;
 
-  let rootOnOff = $.state.rootOnOff;
-  if(rootOnOff == null) rootOnOff = rootObj.getEnabled();
-
-  let blueOnOff = $.state.blueOnOff;
-  if(blueOnOff == null) blueOnOff = blueObj.getEnabled();
-
-  let redOnOff = $.state.redOnOff;
-  if(redOnOff == null) redOnOff = redObj.getEnabled();
-
-  let greenOnOff = $.state.greenOnOff;
-  if(greenOnOff == null) greenOnOff = greenObj.getEnabled();
-
-
-  if(count == 0 || count >= 4){
-    rootObj.setEnabled(false);
-    $.state.rootOnOff = false;
-
-    blueObj.setEnabled(false);
-    $.state.blueOnOff = false;
-    redObj.setEnabled(false);
-    $.state.redOnOff = false;
-    greenObj.setEnabled(false);
-    $.state.greenOnOff = false;
-
-    count = 0
-  }else if(count == 1){
-    rootObj.setEnabled(true);
-    $.state.rootOnOff = true;
-
-    blueObj.setEnabled(true);
-    $.state.blueOnOff = true;
-    redObj.setEnabled(false);
-    $.state.redOnOff = false;
-    greenObj.setEnabled(false);
-    $.state.greenOnOff = false;
-  }else if(count == 2){
-    rootObj.setEnabled(true);
-    $.state.rootOnOff = true;
-
-    blueObj.setEnabled(false);
-    $.state.blueOnOff = false;
-    redObj.setEnabled(true);
-    $.state.redOnOff = true;
-    greenObj.setEnabled(false);
-    $.state.greenOnOff = false;
-  }else if(count == 3){
-    rootObj.setEnabled(true);
-    $.state.rootOnOff = true;
- 
-    blueObj.setEnabled(false);
-    $.state.blueOnOff = false;
-    redObj.setEnabled(false);
-    $.state.redOnOff = false;
-    greenObj.setEnabled(true);
-    $.state.greenOnOff = true;
+  if (count === 0 || count >= 4) {
+    for (const { obj, stateKey } of objStateMap) {
+      settingEnabled(obj, stateKey, false);
+    }
+  } else if (count === 1) {
+    settingEnabled(rootObj, "rootOnOff", true);
+    settingEnabled(blueObj, "blueOnOff", true);
+    settingEnabled(redObj, "redOnOff", false);
+    settingEnabled(greenObj, "greenOnOff", false);
+  } else if (count === 2) {
+    settingEnabled(rootObj, "rootOnOff", true);
+    settingEnabled(blueObj, "blueOnOff", false);
+    settingEnabled(redObj, "redOnOff", true);
+    settingEnabled(greenObj, "greenOnOff", false);
+  } else if (count === 3) {
+    settingEnabled(rootObj, "rootOnOff", true);
+    settingEnabled(blueObj, "blueOnOff", false);
+    settingEnabled(redObj, "redOnOff", false);
+    settingEnabled(greenObj, "greenOnOff", true);
   }
 });
