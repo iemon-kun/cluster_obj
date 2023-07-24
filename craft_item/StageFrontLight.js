edbe9be587e9725396b74e9599e5552c16433e4a
iemon_kun <74810160+iemon-kun@users.noreply.github.com>
2023-07-25 01:44:30 +0900

    Add files via upload

---
+++ craft_item/StageFrontLight.js
@@ -0,0 +1,73 @@
+// 子オブジェクトのstarを取得
+const rootObj = $.subNode("Lightroot");
+const blueObj = $.subNode("Blue");
+const redObj= $.subNode("Red");
+const greenObj = $.subNode("Green");
+
+//初期化
+//let previousHour = null;
+let count = $.state.count;
+if(count == null) count = 0;
+
+//$.log(rand);
+//インタラクト時の処理
+$.onInteract(() => {
+  count += 1;
+
+  let rootOnOff = $.state.rootOnOff;
+  if(rootOnOff == null) rootOnOff = rootObj.getEnabled();
+
+  let blueOnOff = $.state.blueOnOff;
+  if(blueOnOff == null) blueOnOff = blueObj.getEnabled();
+
+  let redOnOff = $.state.redOnOff;
+  if(redOnOff == null) redOnOff = redObj.getEnabled();
+
+  let greenOnOff = $.state.greenOnOff;
+  if(greenOnOff == null) greenOnOff = greenObj.getEnabled();
+
+
+  if(count == 0 || count >= 4){
+    rootObj.setEnabled(false);
+    $.state.rootOnOff = false;
+
+    blueObj.setEnabled(false);
+    $.state.blueOnOff = false;
+    redObj.setEnabled(false);
+    $.state.redOnOff = false;
+    greenObj.setEnabled(false);
+    $.state.greenOnOff = false;
+
+    count = 0
+  }else if(count == 1){
+    rootObj.setEnabled(true);
+    $.state.rootOnOff = true;
+
+    blueObj.setEnabled(true);
+    $.state.blueOnOff = true;
+    redObj.setEnabled(false);
+    $.state.redOnOff = false;
+    greenObj.setEnabled(false);
+    $.state.greenOnOff = false;
+  }else if(count == 2){
+    rootObj.setEnabled(true);
+    $.state.rootOnOff = true;
+
+    blueObj.setEnabled(false);
+    $.state.blueOnOff = false;
+    redObj.setEnabled(true);
+    $.state.redOnOff = true;
+    greenObj.setEnabled(false);
+    $.state.greenOnOff = false;
+  }else if(count == 3){
+    rootObj.setEnabled(true);
+    $.state.rootOnOff = true;
+ 
+    blueObj.setEnabled(false);
+    $.state.blueOnOff = false;
+    redObj.setEnabled(false);
+    $.state.redOnOff = false;
+    greenObj.setEnabled(true);
+    $.state.greenOnOff = true;
+  }
+});
